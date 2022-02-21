6c6cf23981aa305ad8719d71247650ee6be8f5b5
Muskan-movies <muskan1909gupta@gmail.com>
2022-02-21 23:32:37 +0530

    implement backend .

---
+++ backend/router/auth.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+require('../db/conn');
+const User = require('../model/userSchema');
+
+// router.get('/',(req,res) =>{
+//     res.send(`hello this is a home page`);
+// });
+
+//registration route
+router.post('/reg', async (req,res) => {
+
+    const { name, email, password, username, gender, age}=req.body;
+    if( !name || !email || !password || !username || !gender || !age ){
+      return res.status(422).json({error:'error  field not filled properly in regit config --global core.autocrlf truegistration page '});
+    }
+
+    try{
+       const userExist = await User.findOne({ email:email })
+       
+            if(userExist){
+                return res.status(422).json({error:'email id is already exist'});
+            }
+            const userName_Exist = await User.findOne({ username:username});
+            
+                if(userName_Exist){
+                    return res.status(422).json({error:'Username is already exist'});
+                }
+            //for creating collection
+       const user =new User({ name, email, password, username, gender, age});
+       await user.save();
+        res.status(201).json({message:"user register 👍successfull"});
+         
+    }catch(err )
+    {
+        console.log(err);
+    }
+    
+});
+
+//login route
+router.post('/', async (req,res) =>{
+
+   try{
+       const { username , password } =req.body;
+      if( !username || !password)
+    { 
+     return res.status(422).json({error:'error  field not filled properly in login page '});
+    }  
+
+    const userLogin =  await User.findOne({ username:username });
+    if(!userLogin || userLogin.password!=password )
+    {
+        res.status(400).json({error:"user error"});
+    }
+    else
+    {
+       
+         res.json({message:"user login  👍successfully"});
+       
+    }
+   } catch(err)
+   {
+    console.log(err);
+   }
+
+})
+
+
+module.exports = router; 
