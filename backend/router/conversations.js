32a208ef09aab94a98ebe2de69468a58393b3aeb
Muskan-movies <muskan1909gupta@gmail.com>
2022-03-11 22:03:36 +0530

    commit "socket"

---
+++ backend/router/conversations.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const Conversation = require("../model/conversationSchema");
+
+
+router.post("/home-page", async (req, res) => {
+    const newConversation = new Conversation({
+      members: [req.body.senderId, req.body.receiverId],
+    });
+  
+    try {
+      const savedConversation = await newConversation.save();
+      res.status(200).json(savedConversation);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  //get conv of a user
+
+router.get("/:userId", async (req, res) => {
+  try {
+    const conversation = await Conversation.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// get conv includes two userId
+
+router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
+  try {
+    const conversation = await Conversation.findOne({
+      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
+    });
+    res.status(200).json(conversation)
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+  module.exports = router;
+  
